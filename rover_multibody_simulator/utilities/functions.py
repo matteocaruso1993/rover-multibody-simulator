18d88882f8604eed48d859f72dad89e324378409
matteocaruso1993 <70321193+matteocaruso1993@users.noreply.github.com>
2021-03-30 21:46:58 +0200

    Added wrap2pi and findContinguousContactRegions functions

--- rover_multibody_simulator/utilities/functions.py
+++ rover_multibody_simulator/utilities/functions.py
@@ -4,6 +4,7 @@ Created on Thu Mar 25 10:36:38 2021
 
 @author: Matteo
 """
+import numpy as np
 
 def step5(x, x_0, y_0, x_max, y_max):
     """
@@ -44,3 +45,48 @@ def step5(x, x_0, y_0, x_max, y_max):
     
 
     return c
+
+
+
+def wrap2pi(angles):
+    
+    
+    new_angles = (angles + np.pi) % (2*np.pi) -np.pi
+    
+    return new_angles
+
+
+
+
+
+def findContinguousContactRegions(input_array):
+    """
+    Parameters
+    ----------
+    input_array : A numpy array of size (n,)
+        This is an array of booleans.
+
+    Returns
+    -------
+    groups : List of list. Each list represent a set of indices of at least 
+            2 contiguous True elements.
+        
+
+    """
+    groups = list()
+    tmp = list()
+    for i in range(input_array.shape[0]):
+        if input_array[i]:
+            tmp.append(i)
+            
+        else:
+            if tmp and len(tmp)>=2:
+                groups.append(tmp)
+            
+            tmp = list()
+    
+    if len(tmp) >= 2:
+        groups.append(tmp)
+    
+    return groups
+        
