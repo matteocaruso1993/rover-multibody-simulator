6e5435428ba878070a1edec6fbc6fb241a3a5aea
matteocaruso1993 <70321193+matteocaruso1993@users.noreply.github.com>
2021-04-05 23:39:36 +0200

    Updated Friction.py

--- rover_multibody_simulator/utilities/Friction.py
+++ rover_multibody_simulator/utilities/Friction.py
@@ -7,6 +7,9 @@ Created on Mon Apr  5 19:53:43 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import differential_evolution, dual_annealing
+
+from geneticalgorithm import geneticalgorithm as ga
 
 
 class Friction:
@@ -72,6 +75,71 @@ class Friction:
     
     def getFrictionProperties(self):
         return self._friction_properties
+    
+    def setFrictionProperties(self, mu_s, mu_d, v_s, v_d, show_result = False):
+        
+        self._friction_properties['mu static'] = mu_s
+        self._friction_properties['v static'] = v_s
+        
+        self._friction_properties['mu dynamic'] = mu_d
+        self._friction_properties['v dynamic'] = v_d
+        
+        bounds = [(0,1e3),(0,1e3), (0,1e3), (0,1e3)]
+        
+        vel_test = np.linspace(0,v_d,1000)
+        
+        def optimFunction(x):
+            fun = np.zeros((4,), dtype=float)
+            
+            fun[0] = x[0]*(np.tanh(x[1]*v_s)-np.tanh(x[2]*v_s))+ mu_d*np.tanh(x[3]*v_s)-mu_s
+            fun[1] = x[0]*(np.tanh(x[1]*v_d)-np.tanh(x[2]*v_d))+ mu_d*np.tanh(x[3]*v_d)-mu_d
+            vn = v_d + 0.01
+            fun[2] = x[0]*(np.tanh(x[1]*vn)-np.tanh(x[2]*vn))+ mu_d*np.tanh(x[3]*vn)-mu_d
+            vn += 0.01
+            fun[3] = x[0]*(np.tanh(x[1]*vn)-np.tanh(x[2]*vn))+ mu_d*np.tanh(x[3]*vn)-mu_d
+            
+            err = np.sum(fun**2)
+            
+            if x[1] < x[2]:
+                err += 100000
+    
+            if np.any(x < 0):
+                err += 100000
+            
+            g = [x[0], x[1],x[2], mu_d, x[3], 0]
+            
+            self.setCoeffiecients(*g)
+            mu = self.computeFrictionCoeffiecient(vel_test)
+            pos_mu = mu[mu> mu_s]
+            err += np.sum(100*pos_mu)
+                
+            return err
+        
+        
+        #model = differential_evolution(optimFunction, bounds, tol=1e-6, maxiter=10000, popsize=100)
+        model = dual_annealing(optimFunction, bounds, maxiter=10000, x0 = np.ones(4,) )
+        algorithm_param = {'max_num_iteration': 1000,\
+                   'population_size':100,\
+                   'mutation_probability':0.1,\
+                   'elit_ratio': 0.01,\
+                   'crossover_probability': 0.5,\
+                   'parents_portion': 0.3,\
+                   'crossover_type':'uniform',\
+                   'max_iteration_without_improv':None}
+        
+        #model = ga(function = optimFunction, dimension = 4,variable_type = 'real', variable_boundaries=np.array(bounds), algorithm_parameters = algorithm_param)
+        #model.run()
+        
+        g = [model.x[0], model.x[1],model.x[2], mu_d, model.x[3], 0]
+        self.setCoeffiecients(*g)
+        
+        if show_result:
+            self.showFrictionCoefficient()
+        
+        return model
+                
+        
+        
         
         
         
@@ -97,6 +165,7 @@ class Friction:
 if __name__ == '__main__':
     p = [2.0430, 239.1462, 120.0534, 0.5000, 84.4111, 0]
     f = Friction()
-    f.setCoeffiecients(*p)
-    prop = f.getFrictionProperties()
+    r = f.setFrictionProperties(0.8, 0.5, 0.005, 0.02, show_result=True)
+    #f.setCoeffiecients(*p)
+    #prop = f.getFrictionProperties()
         
