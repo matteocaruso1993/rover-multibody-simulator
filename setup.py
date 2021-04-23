5ec3b527f485dc4267239b302d688f4dd3a5926b
matteocaruso1993 <70321193+matteocaruso1993@users.noreply.github.com>
2021-04-23 17:33:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='rover_multibody_simulator',
       license="GPLv3",
       packages=find_packages(),
       package_data = {'rover_multibody_simulator': ['data/config/config.ini','data/video/*.mp4','data/ground/*.json', 'data/model/*.pickle']},
-      install_requires=['sympy','scipy', 'tqdm', 'numpy','matplotlib','dill', 'configparser', 'cloudpickle','jsons','importlib'],
+      install_requires=['sympy','scipy', 'tqdm', 'numpy','matplotlib','dill', 'configparser', 'cloudpickle','jsons','importlib','geneticalgorithm'],
       zip_safe=False,
       include_package_data=True,
       python_requires='>=3.6'
