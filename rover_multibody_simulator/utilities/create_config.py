fb57edc9e18844a5e8bd02642ca467d11c062045
matteocaruso1993 <70321193+matteocaruso1993@users.noreply.github.com>
2021-04-23 17:28:03 +0200

    Add files via upload

--- rover_multibody_simulator/utilities/create_config.py
+++ rover_multibody_simulator/utilities/create_config.py
@@ -38,8 +38,8 @@ config['Model Description']['arm_offset_x'] = '0.1328161' #TO CHECK!
 config['Model Description']['arm_offset_y'] = '0.0' #TO CHECK! # 0 per semplicità
 config['Model Description']['arm_offset_z'] = '0.0' #TO CHECK! #20mm forse, 15.5080mm
 config['Model Description']['arm_cm_distance'] = '-0.004087'
-config['Model Description']['link_lenght'] = '0.1091876'   #TO CHECK!
-config['Model Description']['link_angle'] = str(math.radians(14.44))
+config['Model Description']['link_lenght'] = str('0.11081344461055073')#str(0.1101876+0.002)#'0.1091876'   #TO CHECK!
+config['Model Description']['link_angle'] = str(math.radians(13.818282155900022))    #14.44
 config['Model Description']['hinge1_len'] = str(float(config['Model Description']['link_lenght'])\
                                                 *math.cos(float(config['Model Description']['link_angle']))) #TO CHECK!
 config['Model Description']['hinge1_height'] = str(float(config['Model Description']['link_lenght'])\
@@ -55,9 +55,9 @@ config['Model Description']['swing_arm_inertia_yz'] = str(1.10413*kg_mm2kg_m)
 
 #CENTRAL LINK PROPERTIES
 config['Model Description']['central_link_cm_dist_x'] = '0.0'
-config['Model Description']['central_link_cm_dist_y'] = '0.048808'
+config['Model Description']['central_link_cm_dist_y'] = '0.050808'  #'0.048808'
 config['Model Description']['central_link_cm_dist_z'] = '0.009641'
-config['Model Description']['central_link_lenght'] = '0.09139'
+config['Model Description']['central_link_lenght'] = str(.1)#str(0.09139+0.004)
 config['Model Description']['central_link_mass'] = '0.0766'
 config['Model Description']['central_link_inertia_xx'] = str(39.7567*kg_mm2kg_m)
 config['Model Description']['central_link_inertia_yy'] = str(83.8321*kg_mm2kg_m)
@@ -71,7 +71,7 @@ config['Model Description']['central_link_inertia_yz'] = str(-3.11E-02*kg_mm2kg_
 config['Model Description']['terminal_link_cm_dist_x'] = '0.06471'
 config['Model Description']['terminal_link_cm_dist_y'] = '0.1413'
 config['Model Description']['terminal_link_cm_dist_z'] = '-0.01137'
-config['Model Description']['terminal_link_lenght'] = '0.16639'
+config['Model Description']['terminal_link_lenght'] = str(0.16739+0.002)  #'0.16639'
 config['Model Description']['terminal_link_mass'] = '0.38157'
 config['Model Description']['terminal_link_inertia_xx'] = str(39.7567*kg_mm2kg_m)
 config['Model Description']['terminal_link_inertia_yy'] = str(83.8321*kg_mm2kg_m)
@@ -97,13 +97,13 @@ config['Model Description']['wheel_mass'] = '0.2242'
 config['Model Description']['wheel_inertia_xx'] = str(1108.7749*kg_mm2kg_m)
 config['Model Description']['wheel_inertia_yy'] = str(675.2718*kg_mm2kg_m)
 config['Model Description']['wheel_inertia_zz'] = str(675.2718*kg_mm2kg_m)
-config['Model Description']['wheel_radius'] = '.085'
+config['Model Description']['wheel_radius'] = str(.085+0.005791804035500003) #'.0943709' #'.085'
 
 config['Model Description']['simplified'] = 'False'
 config['Model Description']['debugging'] = 'False'
 config['Model Description']['action-reaction'] = 'True'
-config['Model Description']['initial symbols substitution'] = 'True'
-config['Model Description']['wheel-torque-control-debug'] = 'True'
+config['Model Description']['initial symbols substitution'] = 'False'
+config['Model Description']['wheel-torque-control-debug'] = 'False'
 
 
 #TORSIONAL SPRINGS PROPERTIES
@@ -160,8 +160,8 @@ config['Springs Definition']['c_bl2'] = '5e-3'
 
 
 config.add_section('Ground Properties')
-config['Ground Properties']['stiffness'] = '1000000' #10000
-config['Ground Properties']['damping'] = '100'
+config['Ground Properties']['stiffness'] = '1e6' #10000
+config['Ground Properties']['damping'] = '1e2'
 config['Ground Properties']['exp'] = '1.5'
 config['Ground Properties']['max depth'] = '1e-4'
 
@@ -182,7 +182,7 @@ config['Plotting']['animation fps'] = '30'
 config.add_section('Simulator')
 config['Simulator']['simulation_time'] = '5'
 config['Simulator']['eom_method'] = 'Kane'
-config['Simulator']['integrator'] = 'ipv - RK23' #'ipv - Radau'#'odeint'    #[odeint, ipv - RK45, ipv - Adams, 'Euler-Explicit']
+config['Simulator']['integrator'] = 'ipv - RK23'#ipv - RK23' #'ipv - Radau'#'odeint'    #[odeint, ipv - RK45, ipv - Adams, 'Euler-Explicit']
 config['Simulator']['step size'] = '1e-5'
 
 #Used only for odeint
@@ -191,7 +191,7 @@ config['Simulator']['max stiff order'] = 'auto'
 config['Simulator']['max non-stiff order'] = 'auto'
 config['Simulator']['lambdifier'] = 'lambdify' #options = ['lambdify', theano, autowrap - f2py, autowrap - cython]
 config['Simulator']['autowrap save files'] = 'True'
-config['Simulator']['autowrap save dir'] = 'data/wrappers'
+config['Simulator']['autowrap save dir'] = 'data/wrappers/full_model'
 
 config.add_section('Friction Properties')
 config['Friction Properties']['load_model'] = 'True'
@@ -202,19 +202,40 @@ config['Friction Properties']['g4'] = '0.5'
 config['Friction Properties']['g5'] = '84.4111'
 config['Friction Properties']['g6'] = '0'
 
+config.add_section('Controller')
+config['Controller']['frequency'] = '100'
+
+
 config.add_section('Wheel-Steer-Controller')
-config['Wheel-Steer-Controller']['K1'] = '10'
-config['Wheel-Steer-Controller']['K2'] = '10'
-config['Wheel-Steer-Controller']['K3'] = '10'
-config['Wheel-Steer-Controller']['K4'] = '10'
+config['Wheel-Steer-Controller']['K1'] = '.3'
+config['Wheel-Steer-Controller']['K2'] = '.3'
+config['Wheel-Steer-Controller']['K3'] = '.3'
+config['Wheel-Steer-Controller']['K4'] = '.3'
+config['Wheel-Steer-Controller']['D1'] = '.05'
+config['Wheel-Steer-Controller']['D2'] = '.05'
+config['Wheel-Steer-Controller']['D3'] = '.05'
+config['Wheel-Steer-Controller']['D4'] = '.05'
+config['Wheel-Steer-Controller']['I1'] = '.05'
+config['Wheel-Steer-Controller']['I2'] = '.05'
+config['Wheel-Steer-Controller']['I3'] = '.05'
+config['Wheel-Steer-Controller']['I4'] = '.05'
+
 
 
 
 config.add_section('Wheel-Drive-Controller')
-config['Wheel-Drive-Controller']['K1'] = '10'
-config['Wheel-Drive-Controller']['K2'] = '10'
-config['Wheel-Drive-Controller']['K3'] = '10'
-config['Wheel-Drive-Controller']['K4'] = '10'
+config['Wheel-Drive-Controller']['K1'] = '.1'
+config['Wheel-Drive-Controller']['K2'] = '.1'
+config['Wheel-Drive-Controller']['K3'] = '.1'
+config['Wheel-Drive-Controller']['K4'] = '.1'
+config['Wheel-Drive-Controller']['D1'] = '0'
+config['Wheel-Drive-Controller']['D2'] = '0'
+config['Wheel-Drive-Controller']['D3'] = '0'
+config['Wheel-Drive-Controller']['D4'] = '0'
+config['Wheel-Drive-Controller']['I1'] = '0'
+config['Wheel-Drive-Controller']['I2'] = '0'
+config['Wheel-Drive-Controller']['I3'] = '0'
+config['Wheel-Drive-Controller']['I4'] = '0'
 
 
 
