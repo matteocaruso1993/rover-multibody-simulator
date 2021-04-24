a5bd3e39a7a9cbdb6e7afc5ab9a9ee5db6e94170
matteocaruso1993 <matteo.caruso@phd.units.it>
2021-04-24 14:56:37 +0200

    Debug fixed

--- rover_multibody_simulator/src/__init__.py
+++ rover_multibody_simulator/src/__init__.py
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
 Created on Wed Apr  7 11:34:30 2021
 
 @author: Matteo
 """
-
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  7 11:34:30 2021
-
-@author: Matteo
-"""
-
->>>>>>> b6d4c17251816852f4d7e9bf65d38a15bd77234e
